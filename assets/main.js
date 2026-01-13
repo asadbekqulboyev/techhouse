f916a3fb5e2e26d5435b91686a05db28b81d6a98
asadbek <asadbekqulboyev@gmail.com>
2026-01-13 13:49:23 +0500

    finish

---
+++ assets/main.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FF6600",
+        "background-light": "#FFFFFF",
+        "background-dark": "#0F172A",
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+      borderRadius: {
+        DEFAULT: "12px",
+      },
+    },
+  },
+};
