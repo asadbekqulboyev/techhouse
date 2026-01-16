f915ca625838dc94d7a47e4da90053efb3f14e7e
asadbek <asadbekqulboyev@gmail.com>
2026-01-16 20:55:53 +0500

    finish

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,3 +28,53 @@ let hero_slide = new Swiper(".hero_slide", {
     prevEl: ".next",
   },
 });
+let testimonial_slide = new Swiper(".slide__products", {
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    1280: {
+      slidesPerView: 4,
+      spaceBetween: 50,
+    },
+  },
+  navigation: {
+    nextEl: ".right_action",
+    prevEl: ".left_action",
+  },
+});
+let favoriteCount = 0;
+const favoriteCounter = document.getElementById("favorite_count");
+
+document.querySelectorAll(".product_like").forEach((btn) => {
+  btn.addEventListener("click", function () {
+    const icon = this.querySelector(".material-symbols-outlined");
+
+    // agar hali like qilinmagan bo‘lsa
+    if (!this.classList.contains("liked")) {
+      this.classList.add("liked");
+      icon.classList.add("filled");
+      icon.classList.add("text-primary");
+
+      favoriteCount++;
+    } else {
+      // agar qayta bosilsa (unlike)
+      this.classList.remove("liked");
+      icon.classList.remove("filled");
+      icon.classList.remove("text-primary");
+
+      favoriteCount--;
+    }
+
+    favoriteCounter.textContent = favoriteCount;
+  });
+});
